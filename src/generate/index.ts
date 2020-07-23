81efa5dd3211b43f7774639362e55b22c68dd292
宋运泽 <314851009@qq.com>
2020-07-23 16:58:59 +0800

    代码优化

--- src/generate/index.ts
+++ src/generate/index.ts
@@ -37,6 +37,7 @@ events:[{id:1,event:'click',target:document.documentElement},{id:2,event:'scroll
   start():void{
     // 记录收集信息时间
     this.starttime = new Date().getTime();
+    console.log(this)
     this.width = window.innerWidth;
     this.height = window.innerHeight;
     // 开启事件侦听
@@ -52,7 +53,7 @@ events:[{id:1,event:'click',target:document.documentElement},{id:2,event:'scroll
   startEventListener():void {
     // console.log(this)
     this.events.forEach(ele =>{
-      ele.target.addEventListener(ele.event,this.handler[ele.event]);
+      ele.target.addEventListener(ele.event,this.handler[ele.event].bind(this));
     })
   },
   generateRecord(id:number):Record{
@@ -61,69 +62,72 @@ events:[{id:1,event:'click',target:document.documentElement},{id:2,event:'scroll
     let record = <Record>{
       id,
       time:now,
-      duration: Generate.starttime-now
+      duration:now - this.starttime
     }
     return record;
   },
   handler:{
     // click 处理函数
     'click':function(e:any){
+      console.log(this)
       // console.log
-      const record = generateRecord(1);
+      const record = this.generateRecord(1);
       // click 事件的信息收集
       // console.log(e)
       record.pointer = {left:e.clientX,top:e.clientY}
       record.domPath = Generate.getDompath(e.target)
       console.log(record)
       recordArr.push(record)
+      console.log(recordArr)
     },
     'scroll':function(e:any){
-      const record = generateRecord(2);
+      const record = this.generateRecord(2);
       // scroll 事件的信息收集
       recordArr.push(record)
+      console.log(e)
   
     },
     'touchstart':function(e:any){
-      const record = generateRecord(3);
+      const record = this.generateRecord(3);
       // touchstart 事件的信息收集
       recordArr.push(record)
   
     },
     'touchmove':function(e:any){
-      const record = generateRecord(4);
+      const record = this.generateRecord(4);
       // touchstart 事件的信息收集
       recordArr.push(record)
     },
     'touchend':function(e:any){
-      const record = generateRecord(5);
+      const record = this.generateRecord(5);
       // touchstart 事件的信息收集
       recordArr.push(record)
     },
     'change':function(e:any){
-      const record = generateRecord(6);
+      const record = this.generateRecord(6);
       // touchstart 事件的信息收集
       recordArr.push(record)
     },
     'input':function(e:any){
-      const record = generateRecord(7);
+      const record = this.generateRecord(7);
       // touchstart 事件的信息收集
       recordArr.push(record)
     },
     // 浏览器回退/前进等事件
     'onpopstate':function(e:any){
-      const record = generateRecord(8);
+      const record = this.generateRecord(8);
       // touchstart 事件的信息收集
       recordArr.push(record)
     },
     // 浏览器跳入新路由事件
     'pushState':function(e:any){
-      const record = generateRecord(9);
+      const record = this.generateRecord(9);
       // touchstart 事件的信息收集
       recordArr.push(record)
     },
     // 浏览器替换路由事件
     'replaceState':function(e:any){
-      const record = generateRecord(10);
+      const record = this.generateRecord(10);
       // touchstart 事件的信息收集
       recordArr.push(record)
     }
@@ -135,11 +139,11 @@ events:[{id:1,event:'click',target:document.documentElement},{id:2,event:'scroll
           var r = t.parentElement.childNodes[a];
           (r === t && (i = n),n += 1)
       }
-      var o = t.nodeName.toLowerCase();
-      t.hasAttribute("id") && "" !== t.id ? e.unshift(o + "#" + t.id) : n > 1 ? e.unshift(" > "+ o + ":nth-child(" + i + ")") : e.unshift(o),
+      var o = "> " + t.nodeName.toLowerCase();
+      t.hasAttribute("id") && "" !== t.id ? e.unshift(o + "#" + t.id) : n > 1 ? e.unshift( o + ":nth-child(" + (i+1)+ ")") : e.unshift(o),
       t = t.parentElement
   }
-  return e.slice(1).join(" ")
+  return e.slice(1).join(" ").substring(2);
   }
 }
 
